8e30adddbd0014826acab088ea347fbf636fe3de
Jason Vance <jbvance@gmail.com>
2017-11-27 19:45:37 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -2,7 +2,7 @@ class Book
   attr_accessor :author, :page_count, :genre
   attr_reader :title
 
-  genres = ["Thriller", "Science Fiction", "Romance"]
+  GENRES = ["Thriller", "Science Fiction", "Romance"]
 
   def initialize(title)
     @title = title
